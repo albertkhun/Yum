09010733cbfff6fa96667127e50f654cd7603e2e
Khundongbam Albert Singh <albert.hotmail@icloud.com>
2026-04-10 17:30:38 +0530

    change hosting site to render

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: 'http://localhost:5173',
-  origin: 'https://yumrental.vercel.app',
+  origin: 'https://yumrental2026.onrender.com',
   
   credentials: true,
 }));
