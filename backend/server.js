ed2ce651dc070a3da2649d9b9101916cd52d4a57
Khundongbam Albert Singh <albert.hotmail@icloud.com>
2026-04-26 17:04:46 +0530

    simplify cors setup

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,22 @@ dotenv.config();
 
 const app = express();
 
-//import CORS config
-const corsOptions = require("./config/corsConfig");
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://yumrental2026.onrender.com'
+];
 
-//apply CORS FIRST
-app.use(cors(corsOptions));
-
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
